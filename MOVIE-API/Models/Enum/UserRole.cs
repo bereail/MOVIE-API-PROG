45e814e6ff5133b1d0296285939bffa256062936
BERE <bereailsolohaga@hotmail.com>
2023-12-11 10:34:18 -0300

    m

---
+++ MOVIE-API/Models/Enum/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MOVIE_API.Models.Enum
+{
+    public enum UserRole
+    {
+        [Display(Name = "Admin")]
+        Admin,
+
+        [Display(Name = "Client")]
+        Client
+    }
+}
